8cf88ac68186aabc0e7f564132faed73d6befa72
V.V.Kravchenko <kravczek@gmail.com>
2021-06-06 17:58:44 +0300

    Spring Framework lesson 16 inclusive

---
+++ src/main/java/org/example0606/HelloController.java
@@ -0,0 +1,12 @@
+package org.example0606;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+@GetMapping("/hello")
+    public String sayHello(){
+    return "hello_world";
+}
+}
